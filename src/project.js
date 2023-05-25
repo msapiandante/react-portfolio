300c8bd07e8fbc9f9b9662a59907d2dee647bd0a
Melanie S <melanie.sapiandante@gmail.com>
2023-05-25 11:29:41 -0400

    updated readme.md

--- src/project.js
+++ src/project.js
@@ -9,7 +9,8 @@ function Project(props) {
           <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
             <p className="card-text">{props.description}</p>
-            <a href={props.githublink} className="btn btn-primary">View Code</a>
+            <a href={props.githublink} className="btn btn-primary">View Deployed Application</a>
+            <a href={props.githubRepo} className="btn btn-primary">View Repository</a>
           </div>
       </div>
     </div>
